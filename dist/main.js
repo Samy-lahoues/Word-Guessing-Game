6539d80f4de97cb676904a163fe820565e127cf9
Samy-lahoues <SamyLahoues@outlook.com>
2025-01-19 20:02:49 -0600

    feat: Add hint button functionality

--- dist/main.js
+++ dist/main.js
@@ -18,46 +18,69 @@ let wordToGuess = words[randomNum];
 console.log(wordToGuess)
 let wordToGuessArray = wordToGuess.split("");
 // Manage hints
-hint
+function updateHints(){
+    hintBtn.querySelector("span").innerHTML = `Hints (${numberOfHints})`;
+}
+updateHints();
 // create try and add new try
+
 function addTry () {
     let newTry = '';
     newTry += `
-    <div id="try" class="try space-x-3 justify-between h-20 w-full flex items-center">
+    <div class="try space-x-3 justify-between h-20 w-full flex items-center">
         <span class="font-bold text-2xl w-16">Try ${currentTry}</span>
         <ul class="w-full h-full letters flex space-x-3">
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text"></li>
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
-            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl pt-3 text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text"></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
+            <li class="letter overflow-hidden relative text-black w-20 h-full text-center font-medium text-6xl text-wihte bg-white border-solid border-black border-b-2"><input maxlength="1" class="single-letter w-full absolute left-0 top-1 border-none outline-none text-center" type="text" readonly></li>
         </ul>
     </div>
     `;
     triesContainer.innerHTML += newTry;
+    addEventListenersToTries();
+    tries = document.querySelectorAll(".try");
+    tries[currentTry - 1].querySelector(".letter input").focus();
 }
-tries.forEach(tryElement => {
-    const letters = tryElement.querySelectorAll(".letter input");
-    letters.forEach((letter, index) => {
-        letter.addEventListener("keyup", (event) => {
-            // if a letter was entered
-            if (event.target.value.length === 1){
-                // make current input readonly
-                event.target.setAttribute("readonly", "true");
-            }
-            // Move to the next input if it exsists
-            if (index !== letters.length - 1){
-                let nextLetter = letters[index + 1];
-                nextLetter.removeAttribute("readonly");
-                nextLetter.focus();
-            }
+
+function addEventListenersToTries() {
+    let tries = document.querySelectorAll(".try");
+    tries.forEach(tryElement => {
+        const letters = tryElement.querySelectorAll(".letter input");
+        letters.forEach((letter, index, array) => {
+            letter.addEventListener("keyup", (event) => {
+                // if a letter was entered
+                if (event.target.value.length === 1){
+                    // make current input readonly
+                    event.target.setAttribute("readonly", "true");
+                }
+                // Move to the next input if it exists
+                if (index !== letters.length - 1){
+                    if (!array[index + 1].classList.contains("hint")){
+                        let nextLetter = letters[index + 1];
+                        nextLetter.removeAttribute("readonly");
+                        nextLetter.focus();
+                    }
+                    else if ((index + 2) !== (letters.length - 1) && array[index + 1].classList.contains("hint")){
+                        let nextLetter = letters[index + 2];
+                        nextLetter.removeAttribute("readonly");
+                        nextLetter.focus();
+                    }
+                }
+            })
         })
     })
-})
+}
+
+addEventListenersToTries();
 // Check word button click event
 check.addEventListener("click", () => {
     // Get the current try
+    tries = document.querySelectorAll(".try");
+    console.log(currentTry);
+    console.log(tries[currentTry - 1]);
     let currentInput = tries[currentTry - 1].querySelectorAll(".letter input");
     let currentWordArray = Array.from(currentInput).map(letter => letter.value);
     if (currentWordArray.join("") === wordToGuess){
@@ -85,6 +108,7 @@ check.addEventListener("click", () => {
         }
     }
     if (currentTry !== numberOfTries && currentWordArray.join("") !== wordToGuess){
+        tries[currentTry - 1].style.opacity = "0.5";
         tries[currentTry - 1].querySelectorAll("li").forEach((liElement, index) => {
             liElement.textContent = currentWordArray[index];
             liElement.classList.remove("text-black")
@@ -96,8 +120,28 @@ check.addEventListener("click", () => {
         currentTry++;
         addTry()
     }
-    else if (currentTry === numberOfTries){
+    else if (currentTry === numberOfTries && currentWordArray.join("") !== wordToGuess){
         alert("You Lost");
         location.reload();
     }
 })
+hintBtn.onclick = () => {
+    tries = document.querySelectorAll(".try");
+    if (numberOfHints > 0){
+        let randomIndex = Math.floor(Math.random() * wordToGuessArray.length)
+        let randomLetter = wordToGuessArray[randomIndex];
+        const hintInput = tries[currentTry - 1].querySelectorAll(".letter input");
+        hintInput[randomIndex].classList.add("hint");
+        hintInput[randomIndex].value = randomLetter;
+        hintInput[randomIndex].style.backgroundColor = "yellow";
+        hintInput[randomIndex].style.width = "100%";
+        hintInput[randomIndex].parentElement.style.backgroundColor = "yellow";
+        hintInput[randomIndex].parentElement.style.borderBottom = "none";
+        numberOfHints--;
+        updateHints();
+    }
+    else{
+        alert("You have no more hints");
+        hintBtn.style.display = "none";
+    }
+}
